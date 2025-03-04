1faf03ad07f51fd63e4bb18dbe0c22466aab6d41
allmost <allmostgames@gmail.com>
2025-03-04 18:30:10 +0300

    0.0.1

--- Крестики нолики/MainWindow.xaml.cs
+++ Крестики нолики/MainWindow.xaml.cs
@@ -16,9 +16,43 @@ namespace Крестики_нолики
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool IsFullscreen { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Settings_button_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuGrid.Visibility = Visibility.Collapsed;
+            SettingsPanel.Visibility = Visibility.Visible;
+        }
+
+        private void CloseSettings_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.Visibility = Visibility.Collapsed;
+            MainMenuGrid.Visibility = Visibility.Visible;
+        }
+
+        private void FullscreenCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsFullscreen)
+            {
+                WindowState = WindowState.Maximized;
+                WindowStyle = WindowStyle.None;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+                WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+        }
+
+        private void Exit_button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
